500832a16197785ccf696e009a882774743ede28
Leighton <leighton.l14@gmail.com>
2022-02-27 20:08:21 +1100

    Second Commit - Main File Structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cli-music-app-with-api
+
+## Description
+
+A simple CLI app for saving albums from your favourite artists to your own playlists.
---
+++ config/environment.rb
@@ -0,0 +1,19 @@
+# require 'rake'
+# require 'active_record'
+# require 'yaml/store'
+# require 'ostruct'
+# require 'date'
+require 'bundler/setup'
+Bundler.require
+
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/users.sqlite"
+)
+
+require_all 'app'
